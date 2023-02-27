e5aac2a30bd9501016122010feac476deaef4d75
Seluor <78845320+Seluor@users.noreply.github.com>
2023-02-27 12:55:59 +0700

    Add files via upload

---
+++ Exceptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MatrixCalculator
+{
+    public class InvalidMatrixSizeException : Exception
+    {
+        public InvalidMatrixSizeException(int size) : base($"Invalid matrix size: {size}")
+        {
+        }
+    }
+
+    public class MatrixNotInvertibleException : Exception
+    {
+        public MatrixNotInvertibleException() : base("Matrix is not invertible.")
+        {
+        }
+    }
+}
